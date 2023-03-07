d20a222bfcfd7d2634f25a5804f47fd38055cd8d
sadraw <sadrathescientist@gmail.com>
2023-03-07 21:59:17 +0100

    Data types added

---
+++ Web Dev for Beginners/dataTypes.js
@@ -0,0 +1,20 @@
+// We have 3 different key words here: 
+
+// var
+
+// let
+
+// const
+
+// keyword                  variable name       = "Assigned Value"; 
+// const                    greeting            = "Hello";
+// declaring a variable     
+
+// String : a set of characters that reside between single or double quotes
+
+var keywordOne = "Function scoped - Can be changed in scope -Available before declared!";
+
+let keywordTwo = "Block scoped - Can be changed in scope - Only available after declaration";
+
+const keywordThree = "Block scoped - Cannot be changed - Only available after declaration ";
+
